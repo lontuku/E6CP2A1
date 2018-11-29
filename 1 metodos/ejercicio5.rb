bb092f9165f850f4d4bffeea693d19a513077e11
Tomas K.T <tomas.konig.t@gmail.com>
2018-11-28 21:50:19 -0300

    Ejercicio 5

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,11 @@
 # Crear un método que reciba como parámetro dos números enteros positivos
 # e imprima los números pares que existen entre esos dos números.
+def pares (numero1, numero2)
+	for i in numero1+1..numero2-1
+		if (i % 2).zero?
+			puts i
+		end 		
+	end	
+end	
+
+pares 2, 90
