9a1e76956907d7a52beb2ffb5141730fa6c2bc18
Tomas K.T <tomas.konig.t@gmail.com>
2019-01-21 02:28:35 -0300

    Ej 4 y 5 de arrays corregidos.

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -5,3 +5,27 @@
  # 4. Crear un método que incrementa todos los elementos en una unidad y devuelva un arreglo nuevo.
 
 a = [1,2,3,9,1,4,5,2,3,6,6]
+
+#1
+def eliminar_pares(array)
+	array.delete_if {|ele| ele%2 == 0 }
+end 
+
+def suma(array)
+	sum = 0
+	array.each {|ele| sum += ele}
+	sum
+end
+
+def promedio(array)
+	sum = 0
+	array.each {|ele| sum += ele}
+	sum/array.length
+end
+
+def incremento(array)
+	arr = []
+	array.each {|ele| arr << (ele+1)}
+	arr
+end
+
--- 2 arrays/ejercicio5.rb
+++ 2 arrays/ejercicio5.rb
@@ -8,9 +8,11 @@ products = %w(Producto1 Producto2 Producto3 Producto4)
 prices = %w[1000 2000 1500 950]
 
 html = ''
-products.each do |i|
-  html += "<div class='product'>"
-  html += "</div>\n"
+products.each_with_index do |i, idx|
+	html += "<div class='product'>"
+	html += "<p> #{i} </p>"
+	html += "<p> #{prices[idx]} </p>"
+	html += "</div>\n"
 end
 
 puts html
